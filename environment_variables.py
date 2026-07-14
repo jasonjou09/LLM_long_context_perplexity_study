2f12f5d33a12328c1c2f01368f605c3b3deb5852
jasonjou09 <91236001+jasonjou09@users.noreply.github.com>
2026-07-15 03:17:08 +0800

    Add files via upload

---
+++ environment_variables.py
@@ -0,0 +1,9 @@
+# This file is for storing universal path locations. You can change them here.
+
+# 這是 Intel oneAPI 編譯器 DLL 的預設安裝路徑
+intel_compiler_bin = r"C:\Program Files (x86)\Intel\oneAPI\compiler\latest\bin"
+intel_mkl_bin = r"C:\Program Files (x86)\Intel\oneAPI\mkl\latest\bin"
+
+# 模型路徑 (改成你自己的GGUF檔案的位置)
+model_path_gpt = r"E:\LM Studio\models\lmstudio-community\gpt-oss-20b-GGUF\gpt-oss-20b-MXFP4.gguf"
+model_path_qwen = r"E:\LM Studio\models\mradermacher\Qwen3-4B-Base-GGUF\Qwen3-4B-Base.Q4_K_S.gguf"
